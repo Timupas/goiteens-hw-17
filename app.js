b15b888c0323c35859243177ac1a3335d7c73459
Timupas <164215517+Timupas@users.noreply.github.com>
2026-02-14 10:54:34 +0100

    17hw

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
-const API_KEY = "54251374-33f104c965fd5fe2f8dc6164f";
+const listEl = document.querySelector(".list");
+const btnEl = document.querySelector(".btn");
 
 let page = 1;
-const per_page = 9;
 
-fetch(`https://pixabay.com/api/?key=${API_KEY}&per_page=${per_page}`).then((res) => res.json()).then(console.log(res));
+const API_KEY = "54251374-33f104c965fd5fe2f8dc6164f";
+const PER_PAGE = 9;
 
-const listEl = document.querySelector(".list")
-const btnEl = document.querySelector(".btn")
-const hor = "horizontal"
 
-// fetch(`https://pixabay.com/api/?key=${API_KEY}&page=${page}&per_page=${per_page}`).then(res => res.json()).then(res => console.log(res))
 
 function getPictureApi() {
-    return fetch(`https://pixabay.com/api/?key=${API_KEY}&page=${page}&per_page=${per_page}&orientation=${hor}`).then(res => res.json())
+    return fetch(
+        `https://pixabay.com/api/?key=${API_KEY}&page=${page}&per_page=${PER_PAGE}`
+    ).then(res => res.json());
 }
+getPictureApi().then((res) => createImages(res.hits))
 
-getPictureApi().then(res => createImages(res.hits))
 
-function createImages(array) {
-    const item = array.map(({ largeImageURL, tags }) => {
-        return `
-    <li class="item">
-        <img src="${largeImageURL}" alt="${tags}" class="img">
-    </li>`
-    }).join("")
-    listEl.insertAdjacentHTML("beforeend", item)
+function createImages(arr) {
+    const item = arr.map(({ largeImageURL, tags }) => {
+        return `<li>
+      <img src="${largeImageURL}" alt="${tags}" />
+    </li>`}).join("");
+    listEl.insertAdjacentHTML("beforeend", item);
 }
 
 btnEl.addEventListener("click", onClick)
 
 function onClick() {
-    page += 1
-    getPictureApi().then(res => {
-        if (res.hits.length === 0) {
-            btnEl.disabled = true
-            return
-        }
-        createImages(res.hits)
-    })
+    page += 1;
+    getPictureApi().then((res) => createImages(res.hits))
 }
